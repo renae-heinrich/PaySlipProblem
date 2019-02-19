e84d9ab311f0d0d110c96613b47f41ffc8b8afaa
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-02-19 16:21:53 +1100

    double infinity added

--- src/Main.java
+++ src/Main.java
@@ -104,7 +104,7 @@ public class Main {
         TaxBracket taxBracket2 = new TaxBracket(18201, 37000, 0.19);
         TaxBracket taxBracket3 = new TaxBracket(37001, 87000, 0.325);
         TaxBracket taxBracket4 = new TaxBracket(87001, 180000, 0.37);
-        TaxBracket taxBracket5 = new TaxBracket(180001, 1000000000, 0.45); //not sure how to write > than 180001
+        TaxBracket taxBracket5 = new TaxBracket(180001, Double.POSITIVE_INFINITY, 0.45); 
 
         BigDecimal grossIncome = BigDecimal.valueOf(annualSalary/12).setScale(0, RoundingMode.HALF_UP);
 
@@ -119,7 +119,7 @@ public class Main {
         // want to put into function - need help
 
         BigDecimal incomeTax, netIncome;
-        
+
         if (annualSalary >= taxBracket1.getLowTaxBracket() && annualSalary <= taxBracket1.getHighTaxBracket()) {
             incomeTax = new BigDecimal(0);
             System.out.println("Income Tax: " + incomeTax);
